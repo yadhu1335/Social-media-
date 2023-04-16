9486d668daec1c3674e5ccac23a31ce6523c7827
yadhu1335 <127133557+yadhu1335@users.noreply.github.com>
2023-04-16 20:10:09 +0530

    Added JS file

---
+++ script.js
@@ -0,0 +1,48 @@
+//............SIDEBAR..........
+//............SIDEBAR..........
+const menuItem = document.querySelectorAll('.menu-item');
+
+
+// remove active classlist.....
+const removeActive = ()=>{
+    menuItem.forEach(item => {
+        item.classList.remove('active')        
+    });
+ }
+
+// add active classlist.....
+menuItem.forEach(item => {
+    item.addEventListener('click',()=>{
+        removeActive();
+        item.classList.add('active');
+
+        if(item.id != 'notifice'){
+            document.querySelector('.notification').style.display ='none'
+        }else{
+            document.querySelector('.notification').style.display ='block' 
+            document.querySelector('#notifice .count').style.display='none'
+        }
+    })
+})
+
+
+//....................MESSAGE................
+//....................MESSAGE................
+const message = document.querySelector('#message');
+const messageBox = document.querySelector('#message-box');
+
+message.addEventListener('click',()=>{
+
+    messageBox.classList.add('box-sh');
+    message.querySelector('.count').style.display='none'
+
+
+    setTimeout(() => {
+        messageBox.classList.remove('box-sh');
+    }, 2000);
+
+})
+
+// ...................THEME CUSTOMIZTION......
+// ............THEME CUSTOMIZTION......
+
